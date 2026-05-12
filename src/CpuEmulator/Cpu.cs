a03518f25e7f5b7341084eb81dccbdf6e5df834a
prachwal <prachwal@users.noreply.github.com>
2026-05-13 00:42:45 +0200

    Add minimal instruction execution engine

--- src/CpuEmulator/Cpu.cs
+++ src/CpuEmulator/Cpu.cs
@@ -2,29 +2,122 @@ namespace CpuEmulator;
 
 public class Cpu
 {
+    private readonly Stack<int> _stack = new();
+
     public int[] Registers { get; } = new int[4];
 
     public int[] Memory { get; } = new int[256];
 
+    public int ProgramCounter { get; private set; }
+
+    public bool ZeroFlag { get; private set; }
+
     public bool Halted { get; private set; }
 
-    public void LoadImmediate(int register, int value)
+    public List<Instruction> Program { get; } = new();
+
+    public void LoadProgram(IEnumerable<Instruction> instructions)
     {
-        Registers[register] = value;
+        Program.Clear();
+        Program.AddRange(instructions);
+        ProgramCounter = 0;
+        Halted = false;
     }
 
-    public void Add(int destination, int source)
+    public void Run()
     {
-        Registers[destination] += Registers[source];
+        while (!Halted && ProgramCounter < Program.Count)
+        {
+            Step();
+        }
     }
 
-    public void Store(int register, int address)
+    public void Step()
     {
-        Memory[address] = Registers[register];
-    }
+        var instruction = Program[ProgramCounter];
+        ProgramCounter++;
 
-    public void Halt()
-    {
-        Halted = true;
+        switch (instruction.Opcode)
+        {
+            case Opcode.Nop:
+                break;
+
+            case Opcode.LoadImmediate:
+                Registers[instruction.Operand1] = instruction.Operand2;
+                break;
+
+            case Opcode.Mov:
+                Registers[instruction.Operand1] = Registers[instruction.Operand2];
+                break;
+
+            case Opcode.Load:
+                Registers[instruction.Operand1] = Memory[instruction.Operand2];
+                break;
+
+            case Opcode.Store:
+                Memory[instruction.Operand2] = Registers[instruction.Operand1];
+                break;
+
+            case Opcode.Add:
+                Registers[instruction.Operand1] += Registers[instruction.Operand2];
+                ZeroFlag = Registers[instruction.Operand1] == 0;
+                break;
+
+            case Opcode.Sub:
+                Registers[instruction.Operand1] -= Registers[instruction.Operand2];
+                ZeroFlag = Registers[instruction.Operand1] == 0;
+                break;
+
+            case Opcode.Inc:
+                Registers[instruction.Operand1]++;
+                break;
+
+            case Opcode.Dec:
+                Registers[instruction.Operand1]--;
+                break;
+
+            case Opcode.Cmp:
+                ZeroFlag = Registers[instruction.Operand1] == Registers[instruction.Operand2];
+                break;
+
+            case Opcode.Jump:
+                ProgramCounter = instruction.Operand1;
+                break;
+
+            case Opcode.JumpIfZero:
+                if (ZeroFlag)
+                {
+                    ProgramCounter = instruction.Operand1;
+                }
+                break;
+
+            case Opcode.JumpIfNotZero:
+                if (!ZeroFlag)
+                {
+                    ProgramCounter = instruction.Operand1;
+                }
+                break;
+
+            case Opcode.Push:
+                _stack.Push(Registers[instruction.Operand1]);
+                break;
+
+            case Opcode.Pop:
+                Registers[instruction.Operand1] = _stack.Pop();
+                break;
+
+            case Opcode.Call:
+                _stack.Push(ProgramCounter);
+                ProgramCounter = instruction.Operand1;
+                break;
+
+            case Opcode.Ret:
+                ProgramCounter = _stack.Pop();
+                break;
+
+            case Opcode.Halt:
+                Halted = true;
+                break;
+        }
     }
 }
